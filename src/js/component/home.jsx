1971037bee351aced83167af7121b60ad7d20bb6
SamuelCarmona83 <samuel.carmona.rodrigz@gmail.com>
2024-07-29 23:48:41 -0400

    :tada: Update home.jsx and template.html files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+// parte logica
+const pickRandomItem = (list) => {
+	let numRandom = Math.floor(Math.random() * list.length);
+	return list[numRandom];
+}
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let symbols = ["♦", "♥", "♠", "♣"];
+let values = [
+	"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+];
+
+const cardStyles = {
+	width: "18rem",
+	margin: "auto",
+	height: '400px',
+	fontSize: '86px',
+}
+
+const generateRandomCard = () => {
+
+	const selectedSymbol = pickRandomItem(symbols);
+	return {
+		symbol: selectedSymbol,
+		value: pickRandomItem(values),
+		color: (selectedSymbol === "♦" || selectedSymbol === "♥") ? "red" : "black"
+	};
+}
+
+const Card = () => {
+
+	// parte logica
+	const [card, setCard] = useState(generateRandomCard()); // <------ estado
+
+	// parte visual	
+	return (
+		<div className="d-flex flex-column">
+		<div className="card" style={cardStyles}>
+			<div className="card-body d-flex flex-column justify-content-between">
+				
+				<h5 className="card-title text-start" style={{ fontSize: '76px', color: card.color }}>
+					{card.symbol}
+				</h5>
+
+				<p className="card-text text-center">{card.value}</p>
+
+				<h5 className="card-title text-start"
+					style={{ fontSize: '76px', transform: 'rotate(180deg)', color: card.color }}
+					>
+					{card.symbol}
+				</h5>
+			</div>
+			</div>
+			<button className="btn btn-primary my-2"
+				onClick={() => setCard(generateRandomCard())}
+			>
+				Generate
+			</button>
+		</div>
+	)
+};
 
 //create your first component
 const Home = () => {
+
+	const cards = [ <Card />, <Card />, <Card />, <Card /> ];
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="mx-auto">Random Card Generator</h1>
+			<div className="d-flex flex-wrap gap-2 justify-content-center
+				align-items-center" style={{ height: '100%' }}>
+				{cards}
+			</div>
+		</>
 	);
 };
 
